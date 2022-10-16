3061985e2433af7f53dd0f3376c7da6cbf72c859
墨七 <meichangliang@mo7.cc>
2022-10-16 12:21:24 +0800

    exit-push

--- proxy.js
+++ proxy.js
@@ -1,27 +1,27 @@
 export default {
   '/api': {
-    target: 'http://127.0.0.1:8999',
-    // target: 'https://trade-api.mo7.cc',
+    target: '//127.0.0.1:8999',
+    // target: '//trade-api.mo7.cc',
     changeOrigin: true,
   },
   '/CoinMarket': {
-    target: 'https://trade-api.mo7.cc',
-    // target: 'http://127.0.0.1:8998',
+    target: '//trade-api.mo7.cc',
+    // target: '//127.0.0.1:8998',
     changeOrigin: true,
   },
   '/StockMarket': {
-    target: 'https://trade-api.mo7.cc',
-    // target: 'http://127.0.0.1:8997',
+    target: '//trade-api.mo7.cc',
+    // target: '//127.0.0.1:8997',
     changeOrigin: true,
   },
   '/CoinAI': {
-    target: 'https://trade-api.mo7.cc',
-    // target: 'http://127.0.0.1:9012',
+    target: '//trade-api.mo7.cc',
+    // target: '//127.0.0.1:9012',
     changeOrigin: true,
   },
   '/StockAI': {
-    target: 'https://trade-api.mo7.cc',
-    // target: 'http://127.0.0.1:xxxx',
+    target: '//trade-api.mo7.cc',
+    // target: '//127.0.0.1:xxxx',
     changeOrigin: true,
   },
 };
