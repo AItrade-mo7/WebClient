aea4b6e86ebf60fdd24e57f6fdc11fb33fc41c48
墨七 <meichangliang@mo7.cc>
2022-10-17 20:32:08 +0800

    exit-push

--- src/pages/CoinServe/lib/TradeBtn.vue
+++ src/pages/CoinServe/lib/TradeBtn.vue
@@ -7,25 +7,19 @@ const props = defineProps({
 
 const BuyUp = () => {
   Buy({
-    CoinServeID: props.WssData.CoinServeID,
+    CoinServeID: props.WssData.ServeID,
   });
 };
 
 const SellDown = () => {
   Sell({
-    CoinServeID: props.WssData.CoinServeID,
+    CoinServeID: props.WssData.ServeID,
   });
 };
 
 const CloseAll = () => {
   Close({
-    CoinServeID: props.WssData.CoinServeID,
-  });
-};
-
-const BuySPOT = () => {
-  Buy({
-    CoinServeID: props.WssData.CoinServeID,
+    CoinServeID: props.WssData.ServeID,
   });
 };
 </script>
@@ -35,7 +29,6 @@ const BuySPOT = () => {
     <n-button type="primary" color="#18a058" @click="BuyUp">开多</n-button>
     <n-button type="primary" color="#d03050" @click="SellDown">开空</n-button>
     <n-button type="primary" @click="CloseAll">市价全平</n-button>
-    <n-button type="primary" color="#0c7a43" @click="BuySPOT">现货买入</n-button>
   </div>
 </template>
 
