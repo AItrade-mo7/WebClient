cc495330668d10268c2432c13a26f1fdff76985e
墨七 <meichangliang@mo7.cc>
2022-10-19 17:48:03 +0800

    exit-push

--- src/pages/CoinServe/lib/InfoOk.vue
+++ src/pages/CoinServe/lib/InfoOk.vue
@@ -76,6 +76,7 @@ const HandleKeySubmit = async (type: string, Index: number) => {
           hoverable
           size="small"
         >
+          {{ item }}
           <div class="Server__item">
             <span class="Server__label">ApiKey </span>
             <span class="Server__val">
