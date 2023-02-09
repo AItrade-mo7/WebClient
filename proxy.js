52ee371a9b2456a8f6b9815e1a6e0f7228e73fbe
墨七 <mo7@mo7.cc>
2023-02-09 20:55:31 +0800

    exit-push

--- proxy.js
+++ proxy.js
@@ -1,7 +1,7 @@
 export default {
   '/api': {
-    target: '//127.0.0.1:8999',
-    // target: '//trade-api.mo7.cc',
+    // target: '//127.0.0.1:8999',
+    target: '//trade-api.mo7.cc',
     changeOrigin: true,
   },
   '/CoinMarket': {
