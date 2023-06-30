ad1201d1eb74707bfbb0fc31bb12dddbc3d3f0b8
墨七 <mo7@mo7.cc>
2023-06-30 17:31:13 +0800

    ~,~

--- src/api/Ping.ts
+++ src/api/Ping.ts
@@ -1,4 +1,5 @@
 import { ajax_json } from '@/utils/http';
+import { GetBaseUrl } from '@/config/constant';
 
 export const Ping = (data?: any) => {
   return ajax_json({
@@ -10,7 +11,7 @@ export const Ping = (data?: any) => {
 
 export const GetSysEmailList = () => {
   return ajax_json({
-    BaseUrl: '//msg.mo7.cc',
+    BaseUrl: GetBaseUrl().MsgUrl,
     url: '/api/public/GetSysEmailList',
     data: '',
     method: 'post',
